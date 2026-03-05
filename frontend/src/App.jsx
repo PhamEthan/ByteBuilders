efb8bfd7be7593453d94ec970905e51ccd6057b8
PhamEthan <167038727+PhamEthan@users.noreply.github.com>
2026-03-04 20:30:53 -0600

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,40 @@
-import './css/App.css'
+import "./css/App.css"
 import Calendar from "./pages/Calendar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import FileViewer from "./pages/FileViewer";
-import {Routes, Route} from "react-router-dom"
-import NavBar from "./Components/NavBar";
-import Footer from "./Components/Footer";
-import React, { useState } from "react";
+import Employee from "./pages/Employee";
+import Main from "./layouts/Main";
+import Checkin from "./pages/Checkin";
+import Schedule from "./pages/Schedule";
+import Notes from "./pages/Notes";
 
+import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 
 function App() {
   const [isDark, setDark] = useState(false);
 
   return (
-    <div className="app-container" data-theme={isDark ? "dark" : "light"}> 
-      <NavBar isChecked={isDark} handleChange={() => setDark(!isDark)} />
-      <main className ="main">
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
-          <Route path="/calendar" element={<Calendar />}/>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/fileviewer" element={<FileViewer />}/>
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Routes>
+      
+      {/* Routes WITH Navbar + Footer */}
+      <Route element={<Main isDark={isDark} setDark={setDark} />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/fileviewer" element={<FileViewer />} />
+      </Route>
+
+      {/* Employee Route WITHOUT Navbar + Footer */}
+      <Route path="/employee" element={<Employee />} />
+        <Route path="/checkin" element={<Checkin />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/notes" element={<Notes />} />
+
+    </Routes>
   );
 }
 
