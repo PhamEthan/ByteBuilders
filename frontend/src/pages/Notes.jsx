44c9724fff9778a2e1a15f246051ccbd13a0e687
PhamEthan <167038727+PhamEthan@users.noreply.github.com>
2026-04-22 11:56:54 -0500

    Refactor Notes component to display user tiles

--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,8 +1,20 @@
 import "../css/About.css"
+import UserTile from "../Components/UserTile";
+import { useContext } from "react";
+import { UserContext } from "../contexts/UserContext";
 
 function Notes(){
+    const { users, userStates } = useContext(UserContext);
     return <div className="notes">
-        <h2>No About Us Yet</h2>
+        <h1>Notes</h1>
+        <div className="employee-user-section">
+          <h3>Upcoming Clients</h3>
+          <div className="user-tiles-container">
+            {users.map((user) => (
+              <UserTile key={user.id} {...user} />
+            ))}
+          </div>
+        </div>
     </div>
 }
 
